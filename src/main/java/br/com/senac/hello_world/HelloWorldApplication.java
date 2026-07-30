82f72bc84f0bf24d8dc9818d2ffea3e01c1dfb80
PAULO LEANDRO NETTO CASTRO <paulo.castro@alunos.sc.senac.br>
2026-07-29 21:31:48 -0300

    Primeira alteração

--- src/main/java/br/com/senac/hello_world/HelloWorldApplication.java
+++ src/main/java/br/com/senac/hello_world/HelloWorldApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldApplication {
+	// primeira alteração
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
