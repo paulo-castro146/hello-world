ce24a2e19863c64ef0ded7da47256bcb1c7191bd
paulo-castro146 <66031921+paulo-castro146@users.noreply.github.com>
2026-08-05 19:33:27 -0300

    Update HelloWorldApplication.java

--- src/main/java/br/com/senac/hello_world/HelloWorldApplication.java
+++ src/main/java/br/com/senac/hello_world/HelloWorldApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldApplication {
 
+	// teste
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
